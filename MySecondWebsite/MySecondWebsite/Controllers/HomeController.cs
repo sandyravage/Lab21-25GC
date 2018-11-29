967f2e1a77a77f2a62134c5ec6cca220317aa157
Taylor Everts <tayloreverts@quickenloans.com>
2018-11-29 08:52:35 -0500

    all

---
+++ MySecondWebsite/MySecondWebsite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySecondWebsite.Models;
+
+namespace MySecondWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            
+  
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Title = "Register";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Welcome(Person person)
+        {
+            ViewBag.Title = "Welcome";
+            return View(person);
+        }
+
+        public ActionResult Main()
+        {
+            ViewBag.Title = "Main";
+            return View();
+        }
+    }
+}
---
+++ MySecondWebsite/MySecondWebsite/Scripts/site.js
@@ -0,0 +1,28 @@
+﻿var bingus = document.getElementById("lame");
+
+bingus.addEventListener("input", function (event) {
+    if (bingus.validity.patternMismatch) {
+        bingus.setCustomValidity("Please enter your first name beginning with a capital letter");
+    } else {
+        bingus.setCustomValidity("");
+    }
+});
+
+
+//var slideIndex = 1;
+//showDivs(slideIndex);
+
+//function plusDivs(n) {
+//    showDivs(slideIndex += n);
+//}
+
+//function showDivs(n) {
+//    var i;
+//    var x = document.getElementsByClassName("mySlides");
+//    if (n > x.length) { slideIndex = 1 };
+//    if (n < 1) { slideIndex = x.length };
+//    for (i = 0; i < x.length; i++) {
+//        x[i].style.display = "none";
+//    }
+//    x[slideIndex - 1].style.display = "block";
+//}

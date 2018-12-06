626910996f47d755796f1c30de4180aa2c242b0b
Taylor Everts <tayloreverts@quickenloans.com>
2018-12-06 14:02:06 -0500

    commit2

---
+++ Lab21-26/MySecondWebsite/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySecondWebsite.Models;
+
+namespace MySecondWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            
+  
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Title = "Register";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Welcome(Person person)
+        {
+            ViewBag.Title = "Welcome";
+            return View(person);
+        }
+
+        public ActionResult Main()
+        {
+            ViewBag.Title = "Main";
+            return View();
+        }
+
+        public ActionResult Churches()
+        {
+            ViewBag.Title = "Churches";
+            return View();
+        }
+
+        public ActionResult LRon()
+        {
+            ViewBag.Title = "L Ron Cupboard";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            ViewBag.Title = "FAQ";
+            return View();
+        }
+
+        public ActionResult Today()
+        {
+            ViewBag.Title = "Today";
+            return View();
+        }
+    }
+}
---
+++ Lab21-26/MySecondWebsite/Scripts/site.js
@@ -0,0 +1,56 @@
+﻿var bingus = document.getElementById("lame");
+var bingus2 = document.getElementById("lame2");
+var navs = document.getElementsByTagName("a");
+var bingus3 = document.getElementById("toggle");
+
+bingus3.addEventListener("click", function (event) {
+
+    for (var i = 0; i < navs.length; i++) {
+        if (navs[i].style.display == '') {
+            navs[i].style.display = 'block';
+            bingus3.style.top = '0';
+            bingus3.style.right = '0';
+        }
+        else if (navs[i].style.display == 'block') {
+            navs[i].style.display = '';
+        }
+    }
+});
+
+bingus.addEventListener("input", function (event) {
+    if (bingus.validity.patternMismatch) {
+        bingus.setCustomValidity("Please enter your first name beginning with a capital letter");
+    } else {
+        bingus.setCustomValidity("");
+    }
+});
+
+bingus2.addEventListener("input", function (event) {
+    if (bingus2.validity.patternMismatch) {
+        bingus2.setCustomValidity("Please enter your last name beginning with a capital letter");
+    } else {
+        bingus2.setCustomValidity("");
+    }
+});
+
+function Scrimmy() {
+    
+};
+
+window.onload = function () {
+    document.getElementById("lame11").onchange = validatePassword;
+    document.getElementById("lame12").onchange = validatePassword;
+}
+function validatePassword() {
+    var pass2 = document.getElementById("lame12").value;
+    var pass1 = document.getElementById("lame11").value;
+    if (pass1 != pass2)
+        document.getElementById("lame12").setCustomValidity("Passwords do not match");
+    else
+        document.getElementById("lame12").setCustomValidity('');
+    //empty string means no validation error
+}
+
+
+
+
